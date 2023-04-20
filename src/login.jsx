618172595baf48e00e1aedaa5ec78bd0c04f8fb1
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-04-19 22:56:26 -0500

    update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,2 +1,3 @@
-const login = (email) => {if (/\S+@\S+.\S+/.test(email)) return true;   else return false; };  
+const expresion = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/
+const login = (email) => {if (expresion.test(email)) return true;   else return false; };  
 module.exports = login;
