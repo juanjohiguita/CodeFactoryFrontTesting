5dd4b123220b1009eb6e0fd3db1f633b43f17487
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-04-19 20:35:48 -0500

    Test Configure

---
+++ src/test/login.js
@@ -0,0 +1,1 @@
+const login = (email) => {   if (/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(email)) return true;   else return false; };  module.exports = login;
---
+++ src/test/login.test.js
@@ -0,0 +1,22 @@
+const login = require("./login");
+
+const email1 = "test1234@gmail.com";
+const email2 = "asdas";
+const email3 = "12312@d";
+const email4 = "qweqweq@dds.c";
+
+test("Login exitoso", () => {
+  expect(login(email1)).toBe(true);
+});
+
+test("Login fallido", () => {
+  expect(login(email2)).toBe(false);
+});
+
+test("Login fallido", () => {
+  expect(login(email3)).toBe(false);
+});
+
+test("Login fallido", () => {
+  expect(login(email4)).toBe(false);
+});
