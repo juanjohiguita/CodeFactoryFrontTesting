070162fff0848a78a9b2855b913999078afbdc29
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-04-19 23:35:51 -0500

    Reforma funcionando login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,5 +1,5 @@
-export const validEmail = new RegExp(
-    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+const validEmail = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,3}$'
  );
 
 const login = (email) => {if (validEmail.test(email)) return true;   else return false; };  
