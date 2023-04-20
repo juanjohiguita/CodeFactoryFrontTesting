cde314a861db361e4467059b69c1adfdd9e521ae
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-04-20 00:10:19 -0500

    update password.jsx

--- src/password.jsx
+++ src/password.jsx
@@ -1,3 +1,2 @@
-//const validPassword = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\S]{8,}$');
-const password = (password) => { if ((password.length>=6)) return true;   else return false; }; 
+const password = (password) => { if (password.length>=6) return true;   else return false; }; 
 module.exports = password;
